f3db6e4dcbddd5cee6e044c447a1e26a31a0505d
Joshua Clarke <josh.clarke11@gmail.com>
2025-01-18 11:59:25 -0800

    removed unneeded commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,16 +4,12 @@ import * as deleteCampaign from "./delete-campaign";
 import * as deleteRecap from "./delete-recap";
 import * as listCampaigns from "./list-campaigns";
 import * as ping from "./ping";
-import * as recap from "./recap";
-import * as recaps from "./recaps";
 import * as updateInfo from "./update-info";
 
 export const commands = {
   [ping.data.name]: ping,
   [createCampaign.data.name]: createCampaign,
   [updateInfo.data.name]: updateInfo,
-  [recap.data.name]: recap,
-  [recaps.data.name]: recaps,
   [listCampaigns.data.name]: listCampaigns,
   [deleteRecap.data.name]: deleteRecap,
   [deleteCampaign.data.name]: deleteCampaign,
